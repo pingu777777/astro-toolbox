ce68f195c8f76e43c94e50c58747a2859a25b22f
pingu777777 <162823280+pingu777777@users.noreply.github.com>
2024-04-12 17:38:39 +0200

    Delete cypress/e2e directory

--- cypress/e2e/basic.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Basic specs', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
-  it('Check for jokes link', () => {
-    cy.get('.jokes-link').should('have.attr', 'href').and('include', 'api/joke')
-  })
-})
